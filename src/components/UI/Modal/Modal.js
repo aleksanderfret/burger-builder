2bf0eee737aa4064ad52bce9f3f85c049f435eb4
Aleksander Fret <alemik@op.pl>
2018-08-21 13:28:52 +0200

    move layout component to hoc folder

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Backdrop from '../Backdrop/Backdrop.js';
 import classes from './Modal.css';
 
-const modal = (props) => (
-  <React.Fragment>
-  <div
-    className={classes.Modal}
-    style={{
-      transform: props.show ? 'translate(0)' : 'translateY(-100vh)',
-      opacity: props.show ? '1' : '0'
-    }}>
-    {props.children}
-  </div>
-  <Backdrop
-    show={props.show}
-    clicked={props.modalClosed}/>
-  </React.Fragment>
-);
+class Modal extends Component {
 
-export default modal;
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translate(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+        <Backdrop
+          show={this.props.show}
+          clicked={this.props.modalClosed}/>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Modal;
